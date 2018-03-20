3d93054ba96e3c74114d56b5687930cd15ca23df
liusi <liusi@doumi.com>
2018-03-20 20:10:22 +0800

    增加公共请求方法

--- src/common/util.js
+++ src/common/util.js
@@ -1,20 +1,23 @@
-import zepto from 'zepto'
-const host = 'http://www.doumi.com/api/v1/client'
+// import zepto from 'zepto'
+import 'zepto'
+const host = 'https://api.weixin.qq.com'
 
 const Util = {
 	ajax (ajaxParam) {
 		$.ajax({
 			url : host + ajaxParam.url,
 	        timeout : ajaxParam.timeout,
-	        headers: headersJson,
+	        headers: {
+	        	'Access-Control-Allow-Origin': '*',
+	        },
 	        data: ajaxParam.data,
 	        dataType: ajaxParam.dataType || 'json',
 	        type: ajaxParam.type || 'GET',
 	        success: (data) => {
-
+	        	console.log('success',data)
 	        },
 	        error : (xhr, status) => {
-
+	        	console.log('error',xhr)
 	        }
 		})
 	}
